c4f940e2da250552b38cc80c9fbcbcc43a5ea890
LauraB06 <you@example.com>
2021-02-15 02:42:43 -0300

    chore: atualiza testes

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,6 +1,6 @@
 describe("Home test", () => {
   it("should get title", () => {
-    cy.visit("http://localhost:8080");
+    cy.visit("http://e2e.hcp.com:8080");
     cy.contains("Welcome to Your Vue.js App");
   });
 });
